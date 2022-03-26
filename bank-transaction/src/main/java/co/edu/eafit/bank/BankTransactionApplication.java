164ffa3d6caf9199aad51f234098b789e7332aef
Juan Camilo Acosta <jcamilo016@gmail.com>
2022-03-25 21:21:11 -0500

    prometheus and grafana implemented

--- bank-transaction/src/main/java/co/edu/eafit/bank/BankTransactionApplication.java
+++ bank-transaction/src/main/java/co/edu/eafit/bank/BankTransactionApplication.java
@@ -1,8 +1,11 @@
 package co.edu.eafit.bank;
 
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +15,8 @@ public class BankTransactionApplication {
 		SpringApplication.run(BankTransactionApplication.class, args);
 	}
 
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+		return new TimedAspect(registry);
+	}
 }
--- bank-transaction/src/main/java/co/edu/eafit/bank/controller/BankTransactionController.java
+++ bank-transaction/src/main/java/co/edu/eafit/bank/controller/BankTransactionController.java
@@ -3,13 +3,12 @@ package co.edu.eafit.bank.controller;
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import co.edu.eafit.bank.dto.DepositDTO;
 import co.edu.eafit.bank.dto.TransactionResultDTO;
@@ -20,10 +19,14 @@ import co.edu.eafit.bank.service.BankTransactionService;
 @RestController
 @RequestMapping("/api/v1/transactions")
 @CrossOrigin(origins = "*")
+@RefreshScope
 public class BankTransactionController {
 
 	@Autowired
 	BankTransactionService bankTransactionService;
+
+	@Value("${profile.environment}")
+	String profile;
 
 	@PostMapping("/transfer")
 	@RolesAllowed("ROLE_bank_holder")
@@ -52,8 +55,29 @@ public class BankTransactionController {
 
 	@PostMapping("/unlock")
 	@RolesAllowed("ROLE_cashier")
-	public ResponseEntity<String> unlockAccount() throws Exception {
+	public ResponseEntity<String> unlockAccount() {
 		return ResponseEntity.ok().body("Account unlocked");
 	}
 
+	@GetMapping("/profile")
+	public ResponseEntity<String> getMyProperty(){
+		return ResponseEntity.ok(profile);
+	}
+
+	@GetMapping("very-slow-endpoint")
+	@Timed(value = "very.slow", description = "Time taken to return greeting")
+	public ResponseEntity<String> getVerySlowEndpoint(){
+		try {
+
+			java.util.Random r = new java.util.Random();
+			int low = 0;
+			int high = 30000;
+			int time = r.nextInt(high-low) + low;
+			Thread.sleep(time);
+			return ResponseEntity.ok("Ok");
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body("Error");
+		}
+
+	}
 }
